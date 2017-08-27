40f960cb4839cd2fd75675ad9e7be8919ceedca0
ajinkya933 <ajinkyabobade93@gmail.com>
2017-08-27 11:55:37 -0400

    changed date
    updated information

--- coursebuilder/main.py
+++ coursebuilder/main.py
@@ -1,5 +1,4 @@
 # Copyright 2013 Google Inc. All Rights Reserved.
-# Ajinkya used in 2017
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,8 +9,9 @@
 # distributed under the License is distributed on an "AS-IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
+# 
 # limitations under the License.
-
+# used in 2017
 """Course Builder web application entry point."""
 
 __author__ = 'Pavel Simakov (psimakov@google.com)'
